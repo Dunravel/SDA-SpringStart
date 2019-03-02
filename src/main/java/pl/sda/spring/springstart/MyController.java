e2e397dfea75f568d704cb3f75e7ad6898781159
Marcin Iskra <dunravel@gmail.com>
2019-03-02 10:32:41 +0100

    Created class MyController and hello() method

--- src/main/java/pl/sda/spring/springstart/MyController.java
+++ src/main/java/pl/sda/spring/springstart/MyController.java
@@ -1,4 +1,16 @@
 package pl.sda.spring.springstart;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MyController {
+
+    @RequestMapping(value="test")
+    @ResponseBody
+    public String hello(){
+        return "<b>this is response</b>";
+    }
+
 }
