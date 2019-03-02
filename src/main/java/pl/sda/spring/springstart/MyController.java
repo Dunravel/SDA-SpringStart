ccc8de84ad62d10a35877d42987133c9eec8daea
Marcin Iskra <dunravel@gmail.com>
2019-03-02 11:46:10 +0100

    Added getting directly to class instance in main()

--- src/main/java/pl/sda/spring/springstart/MyController.java
+++ src/main/java/pl/sda/spring/springstart/MyController.java
@@ -1,11 +1,13 @@
 package pl.sda.spring.springstart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
+    @Autowired
     private MyDependency myDependency;
 
     @RequestMapping(value="test")
--- src/main/java/pl/sda/spring/springstart/SpringStartApplication.java
+++ src/main/java/pl/sda/spring/springstart/SpringStartApplication.java
@@ -2,12 +2,16 @@ package pl.sda.spring.springstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringStartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStartApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringStartApplication.class, args);
+        MyDependency myDependency = ctx.getBean(MyDependency.class);
+        System.out.println(myDependency.getResponse());
+        MyDependency myDependency1 = (MyDependency) ctx.getBean("myDep");
+        System.out.println(myDependency1.getResponse());
     }
-
 }
