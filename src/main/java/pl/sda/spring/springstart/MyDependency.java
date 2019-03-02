4c87fdb92c6b59a700e40a5049533139b03aaed5
Marcin Iskra <dunravel@gmail.com>
2019-03-02 10:42:39 +0100

    Created class MyDependency

---
+++ src/main/java/pl/sda/spring/springstart/MyDependency.java
@@ -0,0 +1,9 @@
+package pl.sda.spring.springstart;
+
+public class MyDependency {
+    private String response = "this is data";
+
+    public String getResponse() {
+        return response;
+    }
+}
